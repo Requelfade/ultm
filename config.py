a45025fe121b861c26b83e80fa99f71c7cc77d5a
Requelfade <39383721+Requelfade@users.noreply.github.com>
2020-11-03 20:58:14 +0100

    Create config.py

---
+++ config.py
@@ -0,0 +1,14 @@
+class Config:
+
+    # MySQL Config
+    MYSQL_DATABASE_USER = ''
+    MYSQL_DATABASE_PASSWORD = ''
+    MYSQL_DATABASE_DB = ''
+    MYSQL_DATABASE_HOST = '127..0.1'
+
+    # API Keys
+    RIOT_API_KEY = ''
+
+    # Other
+    DEVELOPMENT = False
+    DEBUG = False
